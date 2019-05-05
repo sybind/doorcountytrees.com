e718e17ffa2163e40f939ae7f21c53b364ee26e8
Dan Wozniak <dan@sybind.com>
2019-05-04 21:16:34 -0700

    configured postgres and docker-compose.yml

--- doorcountytrees/config/dev.exs
+++ doorcountytrees/config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :doorcountytrees, Doorcountytrees.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "docker",
   database: "doorcountytrees_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
